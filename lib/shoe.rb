1170e518def8dd79d2ebbd9915fb908949829e89
Abdul Khan <akhan17@outlook.com>
2020-02-19 18:59:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  Brands = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -15,7 +15,7 @@ class Shoe
   
   def brands=(brands)
     @brands = brands
-    Brands << brands
+    BRANDS << brands
   end
 
 end
